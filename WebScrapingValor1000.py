c25e2c34aa0aba4e4286524cfaeb8c4285c07be7
diogoaraujo <diogorjbr@gmail.com>
2022-06-27 17:11:25 -0300

    comentários para estruturar o código

---
+++ WebScrapingValor1000.py
@@ -0,0 +1,5 @@
+#1. Pegar conteúdo HTML a partir da URL
+#2. Parsear o conteúdo HTML - BeautfulSoup
+#3. Estruturar o conteúdo em um Data Frame - Pandas
+#4. Transformar os Dados em um Dicionário de dados próprio
+#5. Converter e salvar em um arquivo JSON
