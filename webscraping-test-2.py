9318c3ce33464c7be755fb7eb93d23f8c24b4c25
diogoaraujo <diogorjbr@gmail.com>
2022-08-11 14:47:50 -0300

    ws2 extraitabela01

--- webscraping-test-2.py
+++ webscraping-test-2.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import csv
 
+
 #1. Pegar conteúdo HTML a partir da URL
 url = 'https://especial.valor.com.br/valor1000/2021/ranking1000maiores'
-html = pd.read_html(url, match='Sede')
+html = pd.read_html(url, match='Compare')
 
 print(type(html))
-print(html)
 
 ''' with open ('out.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(html) '''
+
+
+print(html)
